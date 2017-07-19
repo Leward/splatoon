08b93c0159b045759f2bdd3b4cf5e904c24d276b
Paul-Julien Vauthier <pj82491@gmail.com>
2017-07-19 20:59:27 +0800

    Cached queries to Twitch

--- grails-app/services/splatoon/website/TwitchService.groovy
+++ grails-app/services/splatoon/website/TwitchService.groovy
@@ -3,22 +3,35 @@ package splatoon.website
 import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.JsonNode
 import com.mashape.unirest.http.Unirest
+import groovy.transform.CompileStatic
 import org.json.JSONObject
 import org.springframework.stereotype.Service
+import splatoon.CachedValue
 import splatoon.TwitchStream
 
+import java.time.Duration
+import java.time.Instant
+
 @Service
+@CompileStatic
 class TwitchService {
 
+    private final CachedValue<List<TwitchStream>> cachedLiveChannels = new CachedValue<>(Duration.ofMinutes(5))
+
+
     List<TwitchStream> getLiveChannels() {
+        if(cachedLiveChannels.cacheValid) {
+            return cachedLiveChannels.value
+        }
         def response = Unirest.get('https://api.twitch.tv/kraken/streams/')
                 .header('Client-ID', '6iml9gxvz80elk7qxjve5ofezklffw')
                 .queryString('game', 'Splatoon')
                 .queryString('stream_type', 'live')
                 .queryString('limit', 8)
                 .asJson()
-        return getJsonObjectsForStreamResponse(response)
-                .collect { new TwitchStream(it) }
+        def streams = getJsonObjectsForStreamResponse(response).collect { new TwitchStream(it) }
+        cachedLiveChannels.cache(streams)
+        return streams
     }
 
     private List<JSONObject> getJsonObjectsForStreamResponse(HttpResponse<JsonNode> response) {
---
+++ src/main/groovy/splatoon/CachedValue.groovy
@@ -0,0 +1,28 @@
+package splatoon
+
+import java.time.Duration
+import java.time.Instant
+
+class CachedValue<T> {
+
+    private T value
+    private Duration timeToLive
+    private Instant expiresAt
+
+    CachedValue(Duration timeToLive) {
+        this.timeToLive = timeToLive
+    }
+
+    void cache(T value) {
+        this.value = value
+        expiresAt = Instant.now().plus(timeToLive)
+    }
+
+    boolean isCacheValid() {
+        return value != null && Instant.now().isBefore(expiresAt)
+    }
+
+    T getValue() {
+        return value
+    }
+}
