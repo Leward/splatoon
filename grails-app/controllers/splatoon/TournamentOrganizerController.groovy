7296358d4bac43f755a4bf48c1b1f3037638abfd
Paul-Julien Vauthier <pj82491@gmail.com>
2017-07-21 00:09:28 +0800

    Only admin can manage tournament organizers

--- grails-app/controllers/splatoon/TournamentOrganizerController.groovy
+++ grails-app/controllers/splatoon/TournamentOrganizerController.groovy
@@ -1,8 +1,11 @@
 package splatoon
 
+import grails.plugin.springsecurity.annotation.Secured
+
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
+@Secured('ROLE_ADMIN')
 @Transactional(readOnly = true)
 class TournamentOrganizerController {
 
