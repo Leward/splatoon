a3590e3eb1cad1362ee82a990805f48dcc3805c6
Paul-Julien Vauthier <pj82491@gmail.com>
2017-11-25 19:32:26 +0800

    Add flank and shot caller roles

--- src/main/groovy/splatoon/PlayerRole.groovy
+++ src/main/groovy/splatoon/PlayerRole.groovy
@@ -4,7 +4,9 @@ enum PlayerRole {
 
     SUPPORT('team.player.role.support'),
     SLAYER('team.player.role.slayer'),
-    FLEX('team.player.role.flex')
+    FLEX('team.player.role.flex'),
+    FLANK('team.player.role.flank'),
+    SHOTCALLER('team.player.role.shotcaller'),
 
     final String i18nMessage
 
