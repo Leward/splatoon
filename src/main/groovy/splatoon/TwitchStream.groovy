aa249e44d67bf0cb31ef1e93ae30b85f9fa376e0
Paul-Julien Vauthier <pj82491@gmail.com>
2017-11-29 22:35:42 +0800

    Use smaller images for channel logo in order to reduce page size

--- src/main/groovy/splatoon/TwitchStream.groovy
+++ src/main/groovy/splatoon/TwitchStream.groovy
@@ -14,6 +14,7 @@ class TwitchStream {
     TwitchStream(JSONObject jsonObject) {
         channelName = jsonObject.getJSONObject('channel').getString('name')
         channelLogoUrl = jsonObject.getJSONObject('channel').optString('logo')
+                .replace('300x300', '70x70') // Get smaller logos
         viewers = jsonObject.getInt('viewers')
         this.game = (jsonObject.getString('game') == 'Splatoon 2') ? Game.SPLATOON_2 : Game.SPLATOON_1
     }
