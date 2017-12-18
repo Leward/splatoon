fbb4b2e4956f63c6f61deedbe460b79211565d99
Paul-Julien Vauthier <pj82491@gmail.com>
2017-12-18 21:21:17 +0800

    Use simpler log format for production

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -12,13 +12,7 @@ conversionRule 'wex', WhitespaceThrowableProxyConverter
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+        pattern = "%level %logger - %msg%n"
     }
 }
 
