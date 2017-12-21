53be960f0396d3de4c6b6c46dfbbe8fd9b5d395b
Paul-Julien Vauthier <pj82491@gmail.com>
2017-12-21 15:47:38 +0800

    Configure logback to send logs to GELF

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -2,35 +2,44 @@ import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
-
 import java.nio.charset.Charset
+import de.siegmar.logbackgelf.GelfUdpAppender
+import de.siegmar.logbackgelf.GelfLayout
+
+// See http://logback.qos.ch/manual/groovy.html for details on configuration
+def env = System.getenv()
+def configuredAppenders = ['STDOUT']
 
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
-
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
-        pattern = "%level %logger - %msg%n"
+        if (Environment.isDevelopmentMode()) {
+            pattern = "%level %logger - %msg%n"
+        } else {
+            pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
+        }
     }
 }
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
+
+if(env['GELF_OVH_TOKEN']) {
+    println "\$GELF_OVH_TOKEN = ${env['GELF_OVH_TOKEN']}"
+    appender('GELF', GelfUdpAppender) {
+        graylogHost = "gra2.logs.ovh.com"
+        graylogPort = 2202
+        layout(GelfLayout) {
+            includeLevelName = true
+            staticFields = ["X-OVH-TOKEN" : env['GELF_OVH_TOKEN']]
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-    logger("splatoon", DEBUG, ['STDOUT'])
-    logger("org.springframework.security", INFO, ['STDOUT'])
-//    logger('org.hibermate.SQL', DEBUG)
-    root(INFO, ['STDOUT', 'FULL_STACKTRACE'])
+    configuredAppenders.add('GELF');
 }
-else {
-    root(INFO, ['STDOUT'])
-}
+
+println "Appenders: ${configuredAppenders}"
+root(INFO, configuredAppenders)
