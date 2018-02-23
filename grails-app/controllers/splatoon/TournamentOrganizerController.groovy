d53442076edb470f0155149bb6a3e2ae01491e02
Paul-Julien Vauthier <pj82491@gmail.com>
2018-02-24 01:22:13 +0800

    Remove defaut limit on list of TO

--- grails-app/controllers/splatoon/TournamentOrganizerController.groovy
+++ grails-app/controllers/splatoon/TournamentOrganizerController.groovy
@@ -12,7 +12,6 @@ class TournamentOrganizerController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
         respond TournamentOrganizer.list(params), model:[tournamentOrganizerCount: TournamentOrganizer.count()]
     }
 
