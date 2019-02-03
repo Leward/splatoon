e7d16f084d8bc26757d0aa61e8bbf87bb537ae14
Paul-Julien Vauthier <pj82491@gmail.com>
2019-02-03 19:54:28 +0800

    Group multiple secrets in single tar

--- ansible/encrypt.sh
+++ ansible/encrypt.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-travis encrypt-file $(dirname "$0")/inventories/production/group_vars/all.yml $(dirname "$0")/inventories/production/group_vars/all.yml.enc
-travis encrypt-file $(dirname "$0")/ssh/id_rsa $(dirname "$0")/ssh/id_rsa.enc
+tar cvf $(dirname "$0")/secrets.tar $(dirname "$0")/inventories/production/group_vars/all.yml $(dirname "$0")/ssh/id_rsa
+
+travis encrypt-file $(dirname "$0")/secrets.tar $(dirname "$0")/secrets.tar.enc --add
+
+#travis encrypt-file $(dirname "$0")/inventories/production/group_vars/all.yml $(dirname "$0")/inventories/production/group_vars/all.yml.enc
+#travis encrypt-file $(dirname "$0")/ssh/id_rsa $(dirname "$0")/ssh/id_rsa.enc
