bce8ecbc37c59086a89d93212827291787b92a65
Paul-Julien Vauthier <pj82491@gmail.com>
2019-02-03 19:59:32 +0800

    ansible ignore ssh fingerprint warning

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 ansible-playbook \
     -i $(dirname "$0")/inventories/production/hosts \
     --private-key=$(dirname "$0")/ssh/id_rsa \
